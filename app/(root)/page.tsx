a661ff76d03bb5a2b8dec4cd8f076f382ebefc63
qhoncheh <ghoncheh.ataee@gmail.com>
2025-01-11 11:47:37 +0330

    startupcard continue👌

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,5 @@
 
-import StartupCard from "@/components/StartupCard";
+import StartupCard from "../../components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({ searchParams}: {searchParams: Promise<{ query?: string }>}) {
@@ -7,12 +7,12 @@ export default async function Home({ searchParams}: {searchParams: Promise<{ que
 
 
   const posts = [{
-    _createdAt: 'yesterday',
+    _createdAt: new Date(),
     views: 55,
-    author: { _id: 1} ,
+    author: { _id: 1 , name: 'Adrian'} ,
     _id: 1,
     description: " This is a description",
-    image: "",
+    // image: "",
     category:"Robots",
     title:"We Robots",
   }]
@@ -34,8 +34,8 @@ export default async function Home({ searchParams}: {searchParams: Promise<{ que
 
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post : StartupCardType, index: number) =>  (
-              <StartupCard />
+            posts.map((post : StartupCardType) =>  (
+              <StartupCard key={posts?._id} post={post}  />
             ))
           ):(
             <p className="no-results">No Startup found</p>
