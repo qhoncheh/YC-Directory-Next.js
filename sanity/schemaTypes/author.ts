c25dba504369b06c6fd8e2d63b0d9459e2fbb7f6
qhoncheh <ghoncheh.ataee@gmail.com>
2025-01-11 20:55:32 +0330

    sanity and structures

--- sanity/schemaTypes/author.ts
+++ sanity/schemaTypes/author.ts
@@ -1,15 +1,40 @@
-export {defineType} from "sanity";
+import { UserIcon } from "lucide-react";
+import { defineField, defineType } from "sanity";
 
-export const author = defineType( schemaDefinition: {
-    name:"author",
-    title:"Autor",
-    type: "document",
-    icon: UserIcon,
-    fields: {
-        defineField ( schemaField: {
-            name: "id",
-            type: "number"
-        }),
-        
-    }
-} )
+export const author = defineType({
+  name: 'author',
+  title: 'Author',
+  type: 'document',
+  icon: UserIcon,
+  fields: [
+    defineField({
+      name: 'id',
+      type: 'number'
+    }),
+    defineField({
+      name: 'name',
+      type: 'string'
+    }),
+    defineField({
+      name: 'username',
+      type: 'string'
+    }),
+    defineField({
+      name: 'email',
+      type: 'string'
+    }),
+    defineField({
+      name: 'image',
+      type: 'url'
+    }),
+    defineField({
+      name: 'bio',
+      type: 'text'
+    })
+  ],
+  preview: {
+    select: {
+      title: "name",
+    },
+  },
+});
